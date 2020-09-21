85fb831dbac9c2a36569bc26869a87f182274461
jojo <joel.mirca@laposte.net>
2020-09-21 16:40:24 +0200

    premiere version

--- App.js
+++ App.js
@@ -1,21 +1,76 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 
+const image ={uri : "https://preppykitchen.com/wp-content/uploads/2019/08/waffle-recipe-.jpg"};
 export default function App() {
   return (
+    <>
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} style={styles.image}>
+        <View>
+        <View style= {styles.marginHeader}>
+        <Text style= {styles.blanc}  >Food Parlour</Text>
+        <Text style= {styles.textCenter}> CAFE - RESTAURANT</Text>
+    </View>
+    <View style= {styles.marginTitle}>
+    <Text style={styles.text}>Name</Text>
+      <Text style={styles.text}>Email</Text>
+      <Text style={styles.text}>Phone</Text>
+      <Button color="#DC143C" title="Sign up"></Button>
+      <Text  >Already a Member ? Login</Text>
+    </View>
+        </View>
+    
+      
+    </ImageBackground>
+      
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    flex:1,
+    resizeMode: "cover",
+    justifyContent:"center",
+  },
+ text:{
+    color: "white",
+    fontSize:18,
+    fontWeight:"regular",
+  },
+  blanc:{
+    color:"white",
+    fontSize:55,
+    fontWeight:"regular",
+    textAlign:"center",
+   
+  },
+  textCenter:{
+    textAlign:"center",
+    fontSize:15,
+    color:"white",
+  
+  },
+  marginHeader:{
+    marginBottom:"8em",
+    marginTop:"4em"
+  },
+
+  marginTitle:{
+    marginBottom:"15em",
+  },
+
+  button:{
+    backgroundColor:"yellow",
+  }
+
 });
